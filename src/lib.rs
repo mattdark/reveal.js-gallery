595eb1ff2e14251886341295fb955606d5de46e5
Mario Garcia <iscmariog@gmail.com>
2018-12-19 18:36:13 -0600

    Function name changed

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub struct Slides {
     pub style: String,
 }
 
-pub fn list_slides() -> Vec<Slides> {
+pub fn get_slides() -> Vec<Slides> {
     let file = File::open("static/slides.json").unwrap(); 
     let reader = BufReader::new(file);
     let slides: Vec<Slides> = serde_json::from_reader(reader).unwrap();
