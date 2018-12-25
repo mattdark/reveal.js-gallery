bee62f50917d8c9cb81f1f4c0125fc1e2a1531ca
Mario Garcia <iscmariog@gmail.com>
2018-12-25 17:18:29 -0600

    Added JS files

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="path"]').onchange=changeEventHandler;
+},false);
+
+function changeEventHandler(event) {
+  url = event.target.value
+  if(url) {
+    $.getJSON("slides.json", function(json) {
+      id = json.find(x => x.url == url).id
+      info = document.getElementById("slide_info");
+      info.style.display = 'block'
+      description = document.getElementById("slide_description");
+      description.innerHTML = json[id - 1].description;
+      screenshot = document.getElementById("slide_screenshot");
+      screenshot.setAttribute('src', json[id - 1].screenshot);
+      pres_title = document.getElementById("slide_title");
+      pres_title.innerHTML = json[id - 1].title;
+    });
+  }
+}
